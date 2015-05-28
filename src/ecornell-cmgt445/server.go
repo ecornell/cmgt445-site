3e30fe43cbd1d7c2e604963b542a1e070743445c
Elijah Cornell <elijah.cornell@gmail.com>
2015-05-27 19:53:07 -0500

    init commit

---
+++ src/ecornell-cmgt445/server.go
@@ -0,0 +1,19 @@
+// main.go
+package main
+
+import (
+	"github.com/go-martini/martini"
+	"github.com/martini-contrib/render"
+)
+
+func main() {
+	m := martini.Classic()
+	// render html templates from templates directory
+	m.Use(render.Renderer())
+
+	m.Get("/", func(r render.Render) {
+		r.HTML(200, "hello", "Billy Bob")
+	})
+
+	m.Run()
+}
