aabb4cb214700a9a3fbb3ef9d1fd805e206f528e
Elijah Cornell <elijah.cornell@gmail.com>
2015-05-28 22:05:31 -0500

    server finished started adding content - imp plan

--- src/ecornell-cmgt445/server.go
+++ src/ecornell-cmgt445/server.go
@@ -1,18 +1,52 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/go-martini/martini"
 	"github.com/martini-contrib/render"
 )
 
-func main() {
-	m := martini.Classic()
+type Server struct {
+	*martini.Martini
+	martini.Router
+}
 
-	m.Use(render.Renderer())
-	m.Use(martini.Static("assets"))
+func InitServer() *Server {
+	r := martini.NewRouter()
+	m := martini.New()
+	m.Use(martini.Recovery())
+	m.Use(martini.Static("assets", martini.StaticOptions{
+		SkipLogging: true,
+	}))
+	m.MapTo(r, (*martini.Routes)(nil))
+	m.Action(r.Handle)
+	m.Use(render.Renderer(render.Options{
+		Layout:  "master",
+		Charset: "UTF-8",
+	}))
+	return &Server{m, r}
+}
+
+func main() {
+
+	fmt.Println("----------------------------------------------")
+	fmt.Println(" CMGT/445 - Wk4 - Implementation Plan Website")
+	fmt.Println(" Elijah Cornell - 2015-06-01")
+	fmt.Println("")
+	fmt.Println(" Site URL:  http://localhost:3000")
+	fmt.Println("----------------------------------------------")
+
+	m := InitServer()
 
 	m.Get("/", func(r render.Render) {
-		r.HTML(200, "hello", "Billy Bob")
+
+		vars := map[string]string{
+			"title": "Home",
+		}
+
+		r.HTML(200, "home", vars)
+
 	})
 
 	m.Run()
