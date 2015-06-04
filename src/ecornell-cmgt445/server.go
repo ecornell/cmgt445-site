6a0d7651233e98b35f2bf11f55dc30a61f1c1d76
Elijah Cornell <elijah.cornell@gmail.com>
2015-06-04 11:30:19 -0500

    started adding bug and change request forms

--- src/ecornell-cmgt445/server.go
+++ src/ecornell-cmgt445/server.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/go-martini/martini"
+	"github.com/martini-contrib/binding"
 	"github.com/martini-contrib/render"
 )
 
@@ -16,6 +17,7 @@ func InitServer() *Server {
 	r := martini.NewRouter()
 	m := martini.New()
 	m.Use(martini.Recovery())
+	// m.Use(martini.Logger())
 	m.Use(martini.Static("assets", martini.StaticOptions{
 		SkipLogging: true,
 	}))
@@ -31,8 +33,8 @@ func InitServer() *Server {
 func main() {
 
 	fmt.Println("----------------------------------------------")
-	fmt.Println(" CMGT/445 - Wk4 - Implementation Plan Website")
-	fmt.Println(" Elijah Cornell - 2015-06-01")
+	fmt.Println(" CMGT/445 - Wk5 - Implementation Plan Website")
+	fmt.Println(" Elijah Cornell - 2015-06-08")
 	fmt.Println("")
 	fmt.Println(" Site URL:  http://localhost:3000")
 	fmt.Println("")
@@ -74,6 +76,92 @@ func main() {
 			"title": "About",
 		}
 		r.HTML(200, "about", vars)
+	})
+
+	m.Get("/bug", func(r render.Render) {
+		vars := map[string]string{
+			"title": "Submit Bug",
+		}
+		r.HTML(200, "bug", vars)
+	})
+
+	type BugForm struct {
+		Description string `form:"desc"`
+		Severity    string `form:"severity"`
+		Steps       string `form:"steps"`
+	}
+
+	m.Post("/bug", binding.Bind(BugForm{}), func(bug BugForm, r render.Render) {
+
+		vars := map[string]string{
+			"title": "Submit Bug",
+		}
+
+		if len(bug.Description) == 0 {
+			vars["error"] = "Short Desciption Is Required"
+
+			fmt.Println("----------------------------------------------")
+			fmt.Println(" Bug Report - Error")
+		}
+
+		if vars["error"] == "" {
+			vars["note"] = "Bug Report Submitted Successfully"
+
+			fmt.Println("----------------------------------------------")
+			fmt.Println(" Bug Report - Success")
+			fmt.Println("   Description: " + bug.Description)
+			fmt.Println("   Severity: " + bug.Severity)
+			fmt.Println("   Steps: " + bug.Steps)
+
+		}
+
+		r.HTML(200, "bug", vars)
+	})
+
+	m.Get("/rfc", func(r render.Render) {
+		vars := map[string]string{
+			"title": "Request for Change",
+		}
+		r.HTML(200, "rfc", vars)
+	})
+
+	type RfcForm struct {
+		Title     string `form:"desc"`
+		Requestor string `form:"requestor"`
+		Category  string `form:"category"`
+		Affect    string `form:"affect"`
+
+		Details      string `form:"detail"`
+		Reason       string `form:"reason"`
+		Alternatives string `form:"alternatives"`
+		Risks        string `form:"risks"`
+		Costs        string `form:"costs"`
+		Quality      string `form:"quality"`
+	}
+
+	m.Post("/rfs", binding.Bind(RfcForm{}), func(rfc RfcForm, r render.Render) {
+
+		vars := map[string]string{
+			"title": "Request for Change",
+		}
+
+		if len(rfc.Title) == 0 {
+			vars["error"] = "Title Is Required"
+
+			fmt.Println("----------------------------------------------")
+			fmt.Println(" Bug Report - Error")
+		}
+
+		if vars["error"] == "" {
+			vars["note"] = "RFC Submitted Successfully"
+
+			fmt.Println("----------------------------------------------")
+			fmt.Println(" Bug Report - Success")
+			fmt.Println("   Title: " + rfc.Title)
+
+		}
+
+		r.HTML(200, "rfc", vars)
 	})
 
 	m.Run()
